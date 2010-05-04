dc07a232f4f697ea988d1ce2f2787c6b8dc2d459
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2010-05-04 06:12:30 +0000

    New fixes and changes for seopanel version 2

--- branches/development/seopanel.1.3.0/views/seoplugins/showseoplugins.ctp.php
+++ branches/development/seopanel.1.3.0/views/seoplugins/showseoplugins.ctp.php
@@ -1,5 +1,5 @@
 <div class="Center" style='width:100%;'>
-<div class="col" style="padding-left:8px;padding-right:12px;">
+<div class="col">
 <div class="Block">
 <table width="100%" border="0" cellspacing="0px" cellpadding="0">
 	<tr>
