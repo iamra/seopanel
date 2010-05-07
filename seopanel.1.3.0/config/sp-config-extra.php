e6ecc1d5dd62680d1f89f942f32a445c152a988f
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2010-05-07 07:12:45 +0000

    New fixes and changes for seopanel version 2

--- seopanel.1.3.0/config/sp-config-extra.php
+++ seopanel.1.3.0/config/sp-config-extra.php
@@ -50,8 +50,4 @@ define('SP_PLUGINUPGRADEFILE', 'upgrade.sql');
 
 # The seo panel pagination default
 define('SP_PAGINGNO_DEFAULT', 10);
-
-# Hot Linking prevents
-define('SP_HOTLINKING', 1);
-
 ?>
