5771cc39c79fd17b5298cdddc3b46925d53b0949
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2010-05-11 19:10:16 +0000

    New fixes and changes for seopanel version 2

--- seopanel.1.3.0/install/install.class.php
+++ seopanel.1.3.0/install/install.class.php
@@ -23,10 +23,156 @@
 class Install {
 	
 	# func to check requirements
-	function checkRequirements() {
+	function checkRequirements($error=false) {
+		$phpClass = "green";
+		$phpSupport = "Yes";
+		$phpVersion = phpversion();
+		if(intval($phpVersion) < 6){			
+			$phpClass = "green";
+			$phpSupport = "Yes";
+		}
+		$phpSupport .= " ( PHP $phpVersion )";
+		
+		$mysqlClass = "red";
+		$mysqlSupport = "No";
+		if(function_exists('mysql_query')){
+			$res = mysql_query("select version() as version");
+			$resObj = mysql_fetch_object($res);
+			$mysqlSupport = "Yes ( MySQL " . $resObj->version ." )";
+			$mysqlClass = "green";
+		}
+		
+		$curlClass = "red";
+		$curlSupport = "No";
+		if(function_exists('curl_version')){
+			$version = curl_version();
+			$curlSupport = "Yes ( CURL  {$version['version']} )";
+			$curlClass = "green";
+		}
+		
+		$shorttagClass = "red";
+		$shorttagSupport = "Disabled";
+		if(ini_get('short_open_tag')){
+			$shorttagSupport = "Enabled";
+			$shorttagClass = "green";
+		}
+		
+		$gdClass = "red";
+		$gdSupport = "No";
+		if(function_exists('gd_info')){
+			$version = gd_info();
+			$gdSupport = "Yes ( GD  {$version['GD Version']} )";
+			$gdClass = "green";
+		}
+		
+		$configClass = "red";
+		$configSupport = "Not found";
+		$configFile = '../config/sp-config.php';
+		if(file_exists($configFile)){
+			$configSupport = "Found, Unwritable<br><p class='note'><b>Command:</b> chmod 777 config/sp-config.php</p>";
+			if(is_writable($configFile)){				
+				$configSupport = "Found, Writable";				
+				$configClass = "green";
+			}			
+		}
+		
+	
+		$tmpClass = "red";
+		$tmpSupport = "Not found";
+		$tmpFile = '../tmp';
+		if(file_exists($tmpFile)){
+			$tmpSupport = "Found, Unwritable<br><p class='note'><b>Command:</b> chmod -R 777 tmp/</p>";
+			if(is_writable($tmpFile)){				
+				$tmpSupport = "Found, Writable";				
+				$tmpClass = "green";
+			}			
+		}
+		$errMsg = $error ? "Please fix the following errors to proceed to next step!" : "";
 		?>
-		hiiiii
+		<h1 class="BlockHeader">Welcome to Seo panel Installation</h1>
+		<form method="post">
+		<table width="100%" cellspacing="8px" cellpadding="0px" class="formtab">
+			<tr><th colspan="2" class="header">Installation compatibility</th></tr>
+			<tr><td colspan="2" class="error"><?php echo $errMsg;?></td></tr>
+			<tr>
+				<th>PHP version >= 4.0.0</th>
+				<td class="<?php echo $phpClass;?>"><?php echo $phpSupport;?></td>
+			</tr>
+			<tr>
+				<th>MySQL Support</th>
+				<td class="<?php echo $mysqlClass;?>"><?php echo $mysqlSupport;?></td>
+			</tr>
+			<tr>
+				<th>CURL Support</th>
+				<td class="<?php echo $mysqlClass;?>"><?php echo $curlSupport; ?></td>
+			</tr>
+			<tr>
+				<th>PHP short_open_tag</th>
+				<td class="<?php echo $shorttagClass;?>"><?php echo $shorttagSupport; ?></td>
+			</tr>
+			<tr>
+				<th>GD graphics support</th>
+				<td class="<?php echo $gdClass;?>"><?php echo $gdSupport; ?></td>
+			</tr>
+			<tr>
+				<th>/config/sp-config.php</th>
+				<td class="<?php echo $configClass;?>"><?php echo $configSupport; ?></td>
+			</tr>
+			<tr>
+				<th>/tmp</th>
+				<td class="<?php echo $tmpClass;?>"><?php echo $tmpSupport; ?></td>
+			</tr>
+		</table>
+		<input type="hidden" value="<?php echo $phpClass;?>" name="php_support">
+		<input type="hidden" value="<?php echo $mysqlClass;?>" name="mysql_support">
+		<input type="hidden" value="<?php echo $curlClass;?>" name="curl_support">
+		<input type="hidden" value="<?php echo $shorttagClass;?>" name="short_open_tag">
+		<input type="hidden" value="<?php echo $configClass;?>" name="config">
+		<input type="hidden" value="startinstall" name="sec">
+		<input type="submit" value="Proceed to next step" name="submit" class="button">
+		</form>
 		<?php
+	}
+	
+	function startInstallation($info) {
+		if( ($info['php_support'] == 'red') || ($info['mysql_support'] == 'red') || ($info['curl_support'] == 'red') || ($info['short_open_tag'] == 'red') || ($info['config'] == 'red') ){
+			$this->checkRequirements(true);
+			return;
+		}
+		?>
+		<h1 class="BlockHeader">Database Settings</h1>
+		<form method="post">
+		<table width="100%" cellspacing="8px" cellpadding="0px" class="formtab">
+			<tr><th colspan="2" class="header">Database configuration</th></tr>
+			<tr><td colspan="2" class="error"><?php echo $errMsg;?></td></tr>
+			<tr>
+				<th>Database type:</th>
+				<td>
+					<select name="db_type">
+						<option value="mysql">MySQL</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<th>Database server hostname:</th>
+				<td><input type="text" name="db_host" value="localhost"></td>
+			</tr>
+			<tr>
+				<th>Database name:</th>
+				<td><input type="text" name="db_name" value=""></td>
+			</tr>
+			<tr>
+				<th>Database username:</th>
+				<td><input type="text" name="db_user" value=""></td>
+			</tr>
+			<tr>
+				<th>Database password:</th>
+				<td><input type="text" name="db_pass" value=""></td>
+			</tr>
+		</table>		
+		<input type="submit" value="Proceed to next step" name="submit" class="button">
+		</form>
+		<?php		
 	}
 	
 	function showDefaultHeader() {
@@ -35,7 +181,7 @@ class Install {
 			<head>
 				<title>Seo Panel installation interface</title>
 				<meta name="description" content="Seo Panel installation Steps to install seo control panel for managing seo of your sites.">
-				<link rel="stylesheet" type="text/css" href="/css/screen.css" media="all" />				
+				<link rel="stylesheet" type="text/css" href="install.css" media="all" />				
 			</head>
 			<body>
 				<div class="installdiv">
