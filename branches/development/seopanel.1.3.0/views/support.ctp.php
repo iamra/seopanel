dd6a4e37207dde71e815af0c5f406146b0188c6c
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2010-05-18 21:27:06 +0000

    New fixes and changes for seopanel version 2

--- branches/development/seopanel.1.3.0/views/support.ctp.php
+++ branches/development/seopanel.1.3.0/views/support.ctp.php
@@ -3,17 +3,50 @@
 
 <fieldset>
 <legend>Seo Panel Support System</legend>
-<h1>Seo Panel support system will provide you following services.</h1>
+
+<ul class="infobox">
+	<li>
+		<h1>1000 Directory Package</h1> 
+		<p>
+			To add 1000 free <b>active and verified</b> internet directories to your seo panel directory submission tool.
+			It will help to increase the backlinks of your website.
+			To get the 100 directory list please <a href="<?=SP_DONATE_LINK?>" target="_blank">donate</a> $10 or more to improve the features of seo panel.  
+		</p>		
+		<p>
+			<a href="<?=SP_DONATE_LINK?>" target="_blank">Visit this link to donate money >></a>
+		</p>
+		<br></br>
+	</li>
+	
+	<li>
+		<h1>New Search Engines</h1> 
+		<p>
+			To add 1000 free <b>active and verified</b> internet directories to your seo panel directory submission tool.
+			It will help to increase the backlinks of your website.
+			To get the 100 directory list please <a href="<?=SP_DONATE_LINK?>" target="_blank">donate</a> $10 or more to improve the features of seo panel.  
+		</p>		
+		<p class="visit">
+			<a href="<?=SP_DONATE_LINK?>" target="_blank">Visit this link to donate money</a>
+		</p>
+		<p></p>
+	</li>
+	
+</ul>
+
 <p style="font-size: 13px;">
 	
-	<p>a) <a><b>1000 Directory Package</b></a> - To add 1000 free internet directories to your seo panel directory submission tool. It will help you to increase the backlinks and site rank.</p>
-	<p>b) <a><b>New Search Engines</b></a> - To add new search engines(eg:baidu.com) to your seo panel keyword position checker.</p>
-	<p>c) <a><b>New Seo Tools</b></a> - To create new seo tools suitable for your buisiness in seo panel.</p>
-	<p>d) <a><b>New Seo Plugins</b></a> - To create new seo plugins suitable for your buisiness in seo panel.</p>	
-	<p>e) <a><b>New Skin</b></a> - To add new skin to seo panel for your buisiness needs.</p>
-	<p>f) <a><b>Customization</b></a> - To customize the seo panel tools,plugins and features for your buisiness needs.</p>
-	<p>g) <a><b>Report Bugs</b></a> - To report new bugs about seo panel tools,plugins and features.</p>
-	<p>g) <a><b>Support Tickets</b></a> - To get technical support from seo panel team to setup the seo panel. Eg: To set up cron for keyword position checker.</p>
+	
+	
+	
+	
+	<p>a) <b>1000 Directory Package</b> - To add 1000 free internet directories to your seo panel directory submission tool. It will help you to increase the backlinks and site rank.</p>
+	<p>b) <b>New Search Engines</b> - To add new search engines(eg:baidu.com) to your seo panel keyword position checker.</p>
+	<p>c) <b>New Seo Tools</b> - To create new seo tools suitable for your buisiness in seo panel.</p>
+	<p>d) <b>New Seo Plugins</b> - To create new seo plugins suitable for your buisiness in seo panel.</p>	
+	<p>e) <b>New Skin</b> - To add new skin to seo panel for your buisiness needs.</p>
+	<p>f) <b>Customization</b> - To customize the seo panel tools,plugins and features for your buisiness needs.</p>
+	<p>g) <b>Report Bugs</b> - To report new bugs about seo panel tools,plugins and features.</p>
+	<p>g) <b>Support Tickets</b> - To get technical support from seo panel team to setup the seo panel. Eg: To set up cron for keyword position checker.</p>
 	
 	<p><a href="http://support.seofreetools.net/" target="_blank" title="seo panel support"><img src="<?=SP_IMGPATH?>/submit_ticket.jpg" alt="seo panel support"></a> or Please <a href="http://support.seofreetools.net/" target="_blank" title="seo panel support"><b>click here</b></a> to create new tickets in <b>seo panel support system</b>.</p>	 
 </p>
