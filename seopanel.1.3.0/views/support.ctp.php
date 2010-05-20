e5f19966f093db9144896b465a4e0750620fe618
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2010-05-20 06:26:25 +0000

    New fixes and changes for seopanel version 2

--- seopanel.1.3.0/views/support.ctp.php
+++ seopanel.1.3.0/views/support.ctp.php
@@ -3,18 +3,18 @@
 
 <fieldset>
 <legend>Seo Panel Support System</legend>
-
 <ul class="infobox">
 	<li>
 		<h1>1000 Directory Package</h1> 
 		<p>
-			We providing <b>1000 free active and verified</b> internet directories to add into your seo panel <b>directory submission tool</b>.
-			It will help us to increase the <b>backlinks</b> of our website.
+			We are providing <b>1000 free active and verified</b> internet directory package, to add into your seo panel <b>directory submission tool</b>.
+			It will help you to <b>increase the backlinks</b> of your websites.
+			Actually 1000 directory list is <b>introduced</b> to collect some money for <b>future devlopment</b> of seo panel.
 			To get the 1000 directory list please <a href="<?=SP_DONATE_LINK?>" target="_blank">donate</a> $10 or more to improve the features of seo panel.
 			If you have any questions about 1000 directory package, please <a href="<?=SP_CONTACT_LINK?>" target="_blank">contact us</a> or open a ticket in <a href="<?=SP_SUPPORT_LINK?>" target="_blank">support system</a>.   
 		</p>		
 		<p>
-			<a href="<?=SP_DONATE_LINK?>" target="_blank">Visit this link to donate money >></a>
+			<a href="<?=SP_DONATE_LINK?>" target="_blank">Visit this link to donate to seo panel >></a>
 		</p>
 		<br>
 	</li>
@@ -22,9 +22,9 @@
 	<li>
 		<h1>Local Search Engines Package</h1> 
 		<p>
-			To add <b>local search engine domains</b> of google,yahoo,msn(<b>eg: www.google.de,www.google.fr etc</b>) to seo panel <b>keyword position checker</b> to track your site perfomance locally.
+			Add <b>local search engine domains</b> of google,yahoo,msn(<b>eg: www.google.de,www.google.fr etc</b>) to seo panel <b>keyword position checker</b>, to track your site perfomance locally.
 			To get local search engines package(according to your requirement) please <a href="<?=SP_DONATE_LINK?>" target="_blank">donate</a> $10 or more to improve the features of seo panel.
-			Before sending donation, please <a href="<?=SP_CONTACT_LINK?>" target="_blank">contact us</a> to provide your required search engine domains.
+			Before sending donation, please <a href="<?=SP_CONTACT_LINK?>" target="_blank">contact us</a> to provide us required search engine domain list.
 			<br><br>			  
 			Also to add <b>new search engines(eg:baidu.com)</b> to your seo panel keyword position checker, please <a href="<?=SP_CONTACT_LINK?>" target="_blank">contact us</a> or open a ticket in <a href="<?=SP_SUPPORT_LINK?>" target="_blank">support system</a>. 
 		</p>		
@@ -37,10 +37,10 @@
 	<li>
 		<h1>Seo Panel Plugins</h1> 
 		<p>
-			Add <b>seo plugins</b> to your seo panel for <b>extend the features</b> according to your requirement.
+			Add <b>seo plugins</b> to your seo panel to <b>extend the features</b> according to your requirement.
 			<a href="<?=SP_PLUGINSITE?>" target="_blank">Seo panel plugins</a> are provided by both seo panel and third party sites.
 			You can also easily <b>develop</b> seo plugins for your seo panel.
-			You can <b>submit</b> it to seo panel and we will <b>distribute</b> it around around the globe after our <b>review</b> process.        
+			You can <a href="<?=SP_PLUGINSITE?>" target="_blank">submit</a> your plugin to seo panel and we will <b>distribute</b> it around around the globe after our <b>review</b> process.        
 		</p>		
 		<p>
 			<a href="<?=SP_PLUGINSITE?>" target="_blank">Visit this link to get seo panel plugins >></a>
@@ -71,7 +71,7 @@
 		<h1>Report Bugs</h1> 
 		<p>
 			Report <b>bugs</b> about <b>seo panel tools,plugins and features etc</b> by using below link. 
-			please help us to improve the features.          
+			please help us to improve the features in next versions.          
 		</p>		
 		<p>
 			<a href="<?=SP_SUPPORT_LINK?>" target="_blank">Visit this link to report bugs >></a>
@@ -81,17 +81,56 @@
 </ul>
 </fieldset>
 
+
 <fieldset>
-<legend>Contact Seo Panel Team</legend>
+<legend>Seo Panel Online Resources</legend>
+<ul class="infobox">
+		
+	<li>
+		<h1>Seo Panel Help Guide</h1> 
+		<p>
+			You can view the <a href="<?=SP_HELP_LINK?>" target="_blank">seo panel documentation</a> in the <b>help guide</b>, it contains the <b>documentation</b> for the seo panel tools,plugins and related features.
+			<br>It is the best place in internet to <b>get help</b> about seo panel. We hope that you <b>contribute</b> to the seo panel help guide, if you find errors or things missing.  	          
+		</p>
+		<p>
+			<a href="<?=SP_HELP_LINK?>" target="_blank">Visit seo panel help guide >></a>
+		</p>
+		<br>
+	</li>	
+	<li>
+		<h1>Seo Panel Forum</h1> 
+		<p>
+			A place to discuss about world's first open source <b>seo control panel</b>. 
+			It is the best place to find the <b>answers</b> to <b>your questions</b> about seo panel. 
+			<br>Also you can <b>share</b> your <b>experience</b> while using seo panel for optimizing your websites.			     	          
+		</p>		
+		<p>
+			<a href="<?=SP_FORUM_LINK?>" target="_blank">Visit seo panel forum >></a>
+		</p>
+		<br>
+	</li>
+</ul>	
 </fieldset>
 
-<fieldset>
-<legend>Download Seo Panel</legend>
-</fieldset>
-
 
 <fieldset>
-<legend>Donate for Seo Panel</legend>
+<legend>Donate to Seo Panel</legend>
+<ul class="infobox">		
+	<li>
+		<h1>Donate to Seo Panel - First Open source seo control panel in the world</h1> 
+		<p>
+			<b>Donate</b> to seo panel to support first open source seo control panel in the world.
+			We are planning to <b>add and improve</b> features of seo panel in future. 
+			<br>With all your <b>support</b> only we can achieve our <b>goals</b>.
+			If you feel that seo panel is <b>worth</b> for you, please donate some amount to seo panel team.
+			<br>We will <b>publish</b> your name and site information in <a href="<?=SP_DONATE_LINK?>" target="_blank">donation page</a>, once we get donation.
+		</p>
+		<p>
+			<a href="<?=SP_DONATE_LINK?>" target="_blank">Visit this link to donate to seo panel >></a>
+		</p>
+		<br>
+	</li>
+</ul>
 </fieldset>
 
 </div>
