9cd0825d604c458bf9f162f1b34ca1f9921212ac
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2010-06-06 14:17:42 +0000

    removes old version

--- trunk/views/directory/featuredsubmission.ctp.php
+++
@@ -1,4 +0,0 @@
-<?php echo showSectionHead($sectionHead); ?>
-<div id='subcontent'>
-<iframe src="http://directory.seopanel.in/submit.php?LINK_TYPE=featured" frameborder="0" height='990px' width='750px;'></iframe>
-</div>
