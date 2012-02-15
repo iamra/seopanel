a40e97a3ba5819bec437c50a866f16047b59c1e9
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2012-02-15 21:18:35 +0000

    seo panel 3.1 changes

--- trunk/seopanel/install/data/upgrade.sql
+++ trunk/seopanel/install/data/upgrade.sql
@@ -1,1 +1,3 @@
-UPDATE searchengines SET regex = '<li.*?class="?g.*?<a.*?href="(.*?)".*?>(.*?)<\\/a>.*?<div.*?>(.*?)<cite>' WHERE url LIKE '%google%';
+UPDATE searchengines SET regex= '<li.*?class="?g.*?<a.*?href="\\/url\\?q=(.*?)&amp;sa=U.*?>(.*?)<\\/a>.*?<div.*?>(.*?)<cite>' WHERE url LIKE '%google%';
+
+INSERT INTO texts(`id`, `lang_code`, `category`, `label`, `content`, `changed`) VALUES (NULL, 'en', 'plugin', 'Download Seo Panel Plugins', 'Download Seo Panel Plugins', CURRENT_TIMESTAMP);
