c1fc6d562a4f148da62dd2f0b0d86078f32a2420
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2012-02-18 21:08:58 +0000

    database changes for new version

--- trunk/seopanel/install/data/upgrade.sql
+++ trunk/seopanel/install/data/upgrade.sql
@@ -6,3 +6,4 @@ ALTER TABLE `saturationresults` DROP `yahoo`;
 UPDATE searchengines SET regex= '<li.*?class="?g.*?<a.*?href="\\/url\\?q=(.*?)&amp;sa=U.*?>(.*?)<\\/a>.*?<div.*?>(.*?)<cite>' WHERE url LIKE '%google%';
 
 INSERT INTO texts(`id`, `lang_code`, `category`, `label`, `content`, `changed`) VALUES (NULL, 'en', 'plugin', 'Download Seo Panel Plugins', 'Download Seo Panel Plugins', CURRENT_TIMESTAMP);
+INSERT INTO texts(`id`, `lang_code`, `category`, `label`, `content`, `changed`) VALUES (NULL, 'en', 'settings', 'getallpluginfree', 'Also get all <b>plugins</b> we develop for <b>Free!</b>', CURRENT_TIMESTAMP);
