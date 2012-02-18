26a2204794c7a5b7136fdf5b308432880b64f72a
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2012-02-18 20:51:28 +0000

    to fix the sql injection issues

--- seopanel/libs/controller.class.php
+++ seopanel/libs/controller.class.php
@@ -56,6 +56,7 @@ class Controller extends Seopanel{
 		# to define all system variables
 		$force = false;
 		if (!empty($_GET['lang_code'])) {
+		    $_GET['lang_code'] = addslashes($_GET['lang_code']);
 			$this->assignLangCode(trim($_GET['lang_code']));
 			$_GET['lang_code'] = '';
 			$force = true;
