8e41d47060c7c207b45f95e854f9a9b6d2278374
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2012-05-30 13:57:05 +0000

    Seo Panel 3.2 changes

--- seopanel/views/email/emailnotificationreportgen.ctp.php
+++ seopanel/views/email/emailnotificationreportgen.ctp.php
@@ -1,15 +1,5 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<link rel="stylesheet" type="text/css" href="<?=SP_CSSPATH?>/screen.css" media="all" />
-    <style>
-        <?php
-        $filename =SP_ABSPATH."/css/screen.css";
-        $fp = fopen($filename, "r");    
-        echo fread($fp, filesize($filename));
-        fclose($fp)
-        ?>
-    </style>
-</head>
+<?php echo $this->getViewContent('email/emailhead'); ?>    
 <body>
 <?=$commonTexts['Hello']?> <?=$name?>,<br><br>
 
