c17bd7bd844f4efcad296b7eaa012ad0b2ea45ef
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2013-10-24 20:51:15 +0000

    Seo Panel 3.4 changes for Forgot password option

---
+++ trunk/seopanel/themes/classic/views/common/forgot.ctp.php
@@ -0,0 +1,31 @@
+<div class="Left">
+	<div class="col">
+        <?php echo getRoundTabTop(); ?>
+        <div id="round_content">
+            <div class="Block">
+                <form name="loginForm" method="post" action="<?=SP_WEBPATH?>/login.php?sec=forgot">
+                <input type="hidden" name="sec" value="requestpass">
+                <table width="70%" cellpadding="0" cellspacing="0" class="actionForm" style="margin-top: 20px;">
+                	<tr>
+                		<td>&nbsp;</td>
+                		<th class="main_header" colspan="2"><?=$spText['login']['Forgot password?']?></th>
+                	</tr>
+                	<tr>
+                		<th><?=$spText['login']['Email']?>:</th>
+                		<td><input type="text" name="email" value="<?=$post['email']?>"><?=$errMsg['email']?></td>
+                	</tr>
+                	<tr>
+                		<th>&nbsp;</th>
+                		<td class="actionsBox">
+                			<?php if(!isLoggedIn() && SP_USER_REGISTRATION){ ?>
+                				&nbsp;<input class="button" type="submit" name="login" value="<?=$spText['login']['Request Password']?> &gt;&gt"/>
+                			<?php }?>
+                		</td>
+                	</tr>
+                </table>               
+                </form>
+            </div>
+        </div>
+    	<?php echo getRoundTabBot(); ?>
+	</div>
+</div>
