f3184c27897fbb8c5a37c05d5b08a81c6c288da9
sendtogeo <sendtogeo@gmail.com>
2014-04-03 20:50:50 +0000

    sp 3.5.0 -  creating crawl log classes and features

--- seopanel.v.3.5.0.beta/install/data/upgrade.sql
+++ seopanel.v.3.5.0.beta/install/data/upgrade.sql
@@ -2,3 +2,22 @@
 -- Seo Panel 3.5.0 changes
 --
 UPDATE `searchengines` SET `regex` = '<li.*?<h3><a.*?RU=(.*?)\\/.*?>(.*?)<\\/a><\\/h3>.*?<div.*?>(.*?)<\\/div>' WHERE url like '%yahoo%';
+
+--
+-- Table structure for table `crawl_log`
+--
+
+CREATE TABLE IF NOT EXISTS `crawl_log` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `crawl_type` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'keyword',
+  `ref_id` int(11) unsigned NOT NULL,
+  `crawl_status` tinyint(4) NOT NULL DEFAULT '1',
+  `proxy_id` int(11) unsigned NOT NULL,
+  `log_message` text COLLATE utf8_unicode_ci NOT NULL,
+  `crawl_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `crawl_status` (`crawl_status`),
+  KEY `crawl_type` (`crawl_type`),
+  KEY `ref_id` (`ref_id`),
+  KEY `proxy_id` (`proxy_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
