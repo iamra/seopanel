aea74c89a630a5798319c7c983fd761c1dc49fd1
sendtogeo <sendtogeo@gmail.com>
2014-04-24 21:49:05 +0000

    sp 3.5.0 -  timezone changes

--- seopanel.v.3.5.0.beta/libs/seopanel.class.php
+++ seopanel.v.3.5.0.beta/libs/seopanel.class.php
@@ -52,6 +52,7 @@ class Seopanel{
 		include_once(SP_CTRLPATH.'/user.ctrl.php');
 		include_once(SP_CTRLPATH.'/settings.ctrl.php');
 		include_once(SP_CTRLPATH."/crawllog.ctrl.php");
+		include_once(SP_CTRLPATH.'/timezone.ctrl.php');
 		
 		//header ('Content-type: text / html; charset = utf-8');
 	}	
